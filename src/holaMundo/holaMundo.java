6530c6812ddaa5440c6fd8b5b9e8af90b1fe3aab
Misael Mendoza <misaelmendozagtz@gmail.com>
2022-02-21 17:03:26 -0700

    first commit

---
+++ src/holaMundo/holaMundo.java
@@ -0,0 +1,15 @@
+package holaMundo;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		
+		String objeto = new String ();
+		
+		objeto = "Hola Mundo";
+		
+		System.out.println(objeto);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author miish
+ *
+ */
+module holaMundo {
+	
+	
+}
