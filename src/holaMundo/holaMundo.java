a10a48b2c5e0048b06e7a55d3ffb9f937efbd8dc
mendo <mendo@192.168.5.39>
2022-02-22 17:19:28 -0700

    Adecuaciones dentro de la clase de 21/02/2022

--- src/holaMundo/holaMundo.java
+++ src/holaMundo/holaMundo.java
@@ -1,5 +1,10 @@
 package holaMundo;
 
+import java.time.Duration;
+
+import pattern.command.Command;
+import pattern.command.ExtendedCommand;
+
 public class holaMundo {
 
 	public static void main(String[] args) {
@@ -9,6 +14,10 @@ public class holaMundo {
 		objeto = "Hola Mundo";
 		
 		System.out.println(objeto);
+		
+		Command command = new ExtendedCommand(Duration.parse("pt1s"));
+		System.out.println("Command must run at " + command.timeToRun());
+		command.run();
 
 	}
 
